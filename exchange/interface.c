54fdf88cf6f3f933783aeab97c3fce2e9c3ca3c5
Ben Pan <ybenp8104@outlook.com>
2024-03-12 12:34:06 -0700

    WIP; Broken Code

--- exchange/interface.c
+++ exchange/interface.c
@@ -20,8 +20,13 @@ static struct {
 typedef struct stock {
     char *name;
     char *symbol;
-    int price[N_TIME];
+    int open_price[N_TIME], close_price[N_TIME];
 } stock_t;
+
+static struct {
+    int n, top;
+    stock_t stocks[MAX_STOCKS];
+} ticker;
 
 static struct {
     int n, top;
@@ -29,9 +34,17 @@ static struct {
     const char *text[MAX_NEWS];
 } news; 
 
+static int max(int a, int b) {
+    return a >= b ? a : b;
+}
+
+static int min(int a, int b) {
+    return a <= b ? a : b;
+}
+
 static void news_init() {
     // initialize news
-    news.n = 10;
+    news.n = 10; // at most MAX_NEWS
     news.color = GL_AMBER;
     news.top = 0;
     news.text[0] = "Citadel LLC becomes the world's largest hedge fund.";
@@ -51,20 +64,41 @@ static void data_init() {
 }
 
 static void draw_news(int x, int y) {
+    // x, y are in character units (not pixels)
+    // TODO: Fix case where not enough news / ticker
+
     const static int N_ROWS_REQ = 9, N_COLS_REQ = 40;
     const static int N_NEWS_DISPLAY = 5;
     for (int i = 0; i < N_NEWS_DISPLAY; i++) {
         int ind = (news.top + i) % N_NEWS_DISPLAY; 
         char buf[N_COLS_REQ + 1]; // + 1 for null-terminator
-        snprintf(buf, N_COLS_REQ + 1, "%d) %s", news.top + i, news.text[ind]); 
+        snprintf(buf, N_COLS_REQ + 1, "%02d) %s", news.top + i, news.text[ind]); 
         int x_pix = gl_get_char_height() * (x + 1 + i), y_pix = gl_get_char_width() * y;
         gl_draw_string(x_pix, y_pix, buf, news.color);
     } 
 }
 
+static void draw_ticker(int x, int y) {
+    // x, y are in character units (not pixels)
+    
+    const static int N_ROWS_REQ = 12, N_COLS_REQ = 12;
+    int N_TICKER_DISPLAY = max(10, ticker.n);
+    for (int i = 0; i < N_TICKER_DISPLAY; i++) {
+        int ind = (ticker.top + i) % N_TICKER_DISPLAY;
+        char buf[N_COLS_REQ + 1]; // + 1 for null-terminator
+        int close_price = ticker.stocks[ind].close_price[module.time];
+        int open_price = ticker.stocks[ind].open_price[module.time];
+        int pct_change = (close_price - open_price) * 100 / open_price;
+
+        snprintf(buf, N_COLS_REQ + 1, " %s  %03d%%", ticker.stocks[ind].symbol, pct_change); 
+        int x_pix = gl_get_char_height() * (x + 1 + i), y_pix = gl_get_char_width() * y;
+        gl_draw_string(x_pix, y_pix, buf, (pct_change >= 0 ? GL_GREEN : GL_RED));
+    }
+}
+
 static void draw_all() {
     // draw_graph(3, 12);
-    // draw_ticker(3, 0);
+    draw_ticker(3, 0);
     draw_news(16, 0);
 }
 
